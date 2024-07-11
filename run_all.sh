68f82a3ee06f36fd6ea7eff743501281a982c19f
zahra.amiri <zahra_amiri@comp.iust.ac.ir>
2024-07-11 19:46:03 +0330

    first commit

---
+++ run_all.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+PYTHONPATH=$(pwd)
+
+cecho(){
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"
+    YELLOW="\033[1;33m"
+    # ... ADD MORE COLORS
+    NC="\033[0m" # No Color
+
+    printf "${!1}${2} ${NC}\n"
+}
+
+SEED=22
+
+# xlnet
+# cecho "YELLOW" "Start dirty_amazon_itunes xlnet"
+# python ./src/run_training.py --model_type=xlnet --model_name_or_path=xlnet-base-cased --data_processor=DeepMatcherProcessor --data_dir=dirty_amazon_itunes --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start abt_buy xlnet"
+# python ./src/run_training.py --model_type=xlnet --model_name_or_path=xlnet-base-cased --data_processor=DeepMatcherProcessor --data_dir=abt_buy --train_batch_size=16 --eval_batch_size=16 --max_seq_length=265 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_walmart_amazon xlnet"
+# python ./src/run_training.py --model_type=xlnet --model_name_or_path=xlnet-base-cased  --data_processor=DeepMatcherProcessor --data_dir=dirty_walmart_amazon --train_batch_size=16 --eval_batch_size=16 --max_seq_length=150 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_dblp_acm xlnet"
+# python ./src/run_training.py --model_type=xlnet --model_name_or_path=xlnet-base-cased  --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_acm --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_dblp_scholar xlnet"
+# python ./src/run_training.py --model_type=xlnet --model_name_or_path=xlnet-base-cased  --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_scholar --train_batch_size=16 --eval_batch_size=16 --max_seq_length=128 --num_epochs=15.0 --seed=${SEED}
+
+
+# roBERTa
+# cecho "YELLOW" "Start dirty_amazon_itunes roBERTa"
+# python ~/PA2/src/run_training.py --model_type=roberta --model_name_or_path=roberta-base --data_processor=DeepMatcherProcessor --data_dir=dirty_amazon_itunes --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start abt_buy roBERTa"
+# python ~/PA2/src/run_training.py --model_type=roberta --model_name_or_path=roberta-base --data_processor=DeepMatcherProcessor --data_dir=abt_buy --train_batch_size=16 --eval_batch_size=16 --max_seq_length=265 --num_epochs=15.0 --seed=${SEED}
+
+cecho "YELLOW" "Start dirty_walmart_amazon roBERTa"
+python ./src/run_training.py --model_type=roberta --model_name_or_path=roberta-base --data_processor=DeepMatcherProcessor --data_dir=dirty_walmart_amazon --train_batch_size=16 --eval_batch_size=16 --max_seq_length=150 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_dblp_acm roBERTa"
+# python ~/PA2/src/run_training.py --model_type=roberta --model_name_or_path=roberta-base --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_acm --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_dblp_scholar roBERTa"
+# python ~/PA2/src/run_training.py --model_type=roberta --model_name_or_path=roberta-base --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_scholar --train_batch_size=16 --eval_batch_size=16 --max_seq_length=128 --num_epochs=15.0 --seed=${SEED}
+
+
+# ##XLM
+# #cecho "YELLOW" "Start dirty_amazon_itunes XLM"
+# #python ~/PA2/src/run_training.py --model_type=xlm --model_name_or_path=xlm-mlm-ende-1024 --data_processor=DeepMatcherProcessor --data_dir=dirty_amazon_itunes --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+# #
+# #cecho "YELLOW" "Start abt_buy XLM"
+# #python ~/PA2/src/run_training.py --model_type=xlm --model_name_or_path=xlm-mlm-ende-1024 --data_processor=DeepMatcherProcessor --data_dir=abt_buy --train_batch_size=16 --eval_batch_size=16 --max_seq_length=265 --num_epochs=15.0 --seed=${SEED}
+# #
+# #cecho "YELLOW" "Start dirty_walmart_amazon XLM"
+# #python ~/PA2/src/run_training.py --model_type=xlm --model_name_or_path=xlm-mlm-ende-1024 --data_processor=DeepMatcherProcessor --data_dir=dirty_walmart_amazon --train_batch_size=16 --eval_batch_size=16 --max_seq_length=150 --num_epochs=15.0 --seed=${SEED}
+# #
+# #cecho "YELLOW" "Start dirty_dblp_acm XLM"
+# #python ~/PA2/src/run_training.py --model_type=xlm --model_name_or_path=xlm-mlm-ende-1024 --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_acm --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+# #
+# #cecho "YELLOW" "Start dirty_dblp_scholar XLM"
+# #python ~/PA2/src/run_training.py --model_type=xlm --model_name_or_path=xlm-mlm-ende-1024 --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_scholar --train_batch_size=16 --eval_batch_size=16 --max_seq_length=128 --num_epochs=15.0 --seed=${SEED}
+
+
+# BERT
+# cecho "YELLOW" "Start dirty_amazon_itunes BERT"
+# python ~/PA2/src/run_training.py --model_type=bert --model_name_or_path=pre_trained_model/bert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=dirty_amazon_itunes --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start abt_buy BERT"
+# python ~/PA2/src/run_training.py --model_type=bert --model_name_or_path=pre_trained_model/bert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=abt_buy --train_batch_size=16 --eval_batch_size=16 --max_seq_length=265 --num_epochs=15.0 --seed=${SEED}
+
+cecho "YELLOW" "Start dirty_walmart_amazon BERT"
+python ./src/run_training.py --model_type=bert --model_name_or_path=pre_trained_model/bert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=dirty_walmart_amazon --train_batch_size=16 --eval_batch_size=16 --max_seq_length=150 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_dblp_acm BERT"
+# python ~/PA2/src/run_training.py --model_type=bert --model_name_or_path=pre_trained_model/bert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_acm --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_dblp_scholar BERT"
+# python ~/PA2/src/run_training.py --model_type=bert --model_name_or_path=pre_trained_model/bert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_scholar --train_batch_size=16 --eval_batch_size=16 --max_seq_length=128 --num_epochs=15.0 --seed=${SEED}
+
+# # DistilBERT
+# cecho "YELLOW" "Start dirty_amazon_itunes DistilBERT"
+# python ~/PA2/src/run_training.py --model_type=distilbert --model_name_or_path=distilbert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=dirty_amazon_itunes --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start abt_buy DistilBERT"
+# python ~/PA2/src/run_training.py --model_type=distilbert --model_name_or_path=distilbert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=abt_buy --train_batch_size=16 --eval_batch_size=16 --max_seq_length=265 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_walmart_amazon DistilBERT"
+# python ~/PA2/src/run_training.py --model_type=distilbert --model_name_or_path=distilbert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=dirty_walmart_amazon --train_batch_size=16 --eval_batch_size=16 --max_seq_length=150 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_dblp_acm DistilBERT"
+# python ~/PA2/src/run_training.py --model_type=distilbert --model_name_or_path=distilbert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_acm --train_batch_size=16 --eval_batch_size=16 --max_seq_length=180 --num_epochs=15.0 --seed=${SEED}
+
+# cecho "YELLOW" "Start dirty_dblp_scholar DistilBERT"
+# python ~/PA2/src/run_training.py --model_type=distilbert --model_name_or_path=distilbert-base-uncased --data_processor=DeepMatcherProcessor --data_dir=dirty_dblp_scholar --train_batch_size=16 --eval_batch_size=16 --max_seq_length=128 --num_epochs=15.0 --seed=${SEED}
---
+++ src/read_files.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+# Read the CSV files
+train_df = pd.read_csv('data/dirty_walmart_amazon/deep_matcher/train.csv')
+tableA_df = pd.read_csv('data/dirty_walmart_amazon/deep_matcher/tableA.csv')
+tableB_df = pd.read_csv('data/dirty_walmart_amazon/deep_matcher/tableB.csv')
+
+# Function to get the row from tableA and tableB based on IDs in train file
+def get_matching_rows(train_row, tableA_df, tableB_df):
+    ltable_id = train_row['ltable_id']
+    rtable_id = train_row['rtable_id']
+    
+    # Find the corresponding rows in tableA and tableB
+    tableA_row = tableA_df[tableA_df['id'] == ltable_id]
+    tableB_row = tableB_df[tableB_df['id'] == rtable_id]
+    
+    return tableA_row, tableB_row
+
+
+def apply_template(row):
+    # cols = ['title','category','brand','modelno','price']
+    text = ''
+    if not row['title'].isna().values[0]:
+        text += f'Introducing the {row['title'].values[0]}'
+
+    if not row['category'].isna().values[0] and not row['brand'].values[0]:
+        text += f", a product in the {row['category'].values[0]} category by {row['brand'].values[0]}."
+    elif not row['category'].isna().values[0]:
+        text += f", in the {row['category'].values[0]} category."
+    elif not row['brand'].isna().values[0]:
+        text += f" by {row['brand'].values[0]}."
+        
+    if not row['modelno'].isna().values[0] and not row['price'].isna().values[0]:
+        text += f" This model, {row['modelno'].values[0]}, is available for {row['price'].values[0]}."
+    elif not row['modelno'].isna().values[0]:
+        text += f" Its model is {row['modelno'].values[0]}."
+    elif not row['price'].isna().values[0]:
+        text += f" This product is available for {row['price'].values[0]}."
+
+    return text
+
+
+# Create an empty DataFrame to store the results
+result_df = pd.DataFrame(columns=['idx', 'text_left', 'text_right', 'label'])
+
+# Iterate through each row in the train file and store the corresponding rows in tableA and tableB
+for idx, train_row in train_df.iterrows():
+    tableA_row, tableB_row = get_matching_rows(train_row, tableA_df, tableB_df)
+    
+    # Assuming 'text' is the column of interest in tableA and tableB
+    text_left = apply_template(tableA_row)
+    text_right = apply_template(tableB_row)
+    label = train_row['label']
+    
+    # Create a new DataFrame for the current row
+    new_row = pd.DataFrame([{
+        'idx': idx,
+        'text_left': text_left,
+        'text_right': text_right,
+        'label': label
+    }])
+    
+    # Append the new row to the result DataFrame using pd.concat
+    result_df = pd.concat([result_df, new_row], ignore_index=True)
+
+
+# Write the result DataFrame to a .tsv file
+result_df.to_csv('data/dirty_walmart_amazon/train.tsv', sep='\t', index=False)
+
+# Introducing the [Title], a product in the [Category] category by [Brand]. This model, [Model No.], is available for  [Price].
